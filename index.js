c33d57e3194bd0dbc9f47d545848d20a09f92f19
Scott Atkins <satkins16@gmail.com>
2017-11-11 22:08:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Driver {
 
   yearsExperienceFromBeginningOf(year) {
     let endDate = new Date(year, 1, 1)
-    let totalYears = (endDate - year)/(365*24*60*60*1000)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
     return parseInt(totalYears)
   }
 }
